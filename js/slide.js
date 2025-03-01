d6538c456a4bbb2c0e90cdf7798faf879505cf25
unknown <frankisnaptwinglef@gmail.com>
2025-03-01 13:48:02 -0800

    done

--- js/slide.js
+++ js/slide.js
@@ -24,9 +24,7 @@ function Startside(){
       }, 7000);
 }
 Startside()
-// slide js ends here
-
-
+// slide js ends here 
 // odd start here
 function Odds() { 
      let rightvisual = document.getElementById("rightvisual");
@@ -36,6 +34,53 @@ function Odds() {
      let mainbug = document.getElementById("mainbug");
      let seconviual = document.getElementById("seconviual");
      let lose = document.getElementById("lose");
+     let oneodder = document.getElementById("oneodder");
+     let secodd = document.getElementById("secodd"); 
+     let thirdod = document.getElementById("thirdod")
+     let green = "green"
+     thirdod.addEventListener("click",()=>{
+        if (oneodder.style.backgroundColor == green || secodd.style.backgroundColor==green) {
+           secodd.style.backgroundColor="rgb(207, 207, 207)";
+           secodd.style.color="black";
+           thirdod.style.color="white";
+           thirdod.style.backgroundColor="green"; 
+           oneodder.style.color="black";
+           oneodder.style.backgroundColor="rgb(207, 207, 207)";
+        } 
+        thirdod.style.backgroundColor="green";
+        thirdod.style.color="white"
+     })
+     
+     oneodder.addEventListener("click",()=>{
+       if (secodd.style.backgroundColor== green || thirdod.style.backgroundColor == green ||  oneodder.style.backgroundColor == green ) {
+           secodd.style.backgroundColor="rgb(207, 207, 207)";
+           secodd.style.color="black"
+           thirdod.style.backgroundColor="rgb(207, 207, 207)";
+           thirdod.style.color="black"
+           oneodder.style.backgroundColor="rgb(207, 207, 207)";
+           oneodder.style.color="black";
+       }
+        else{ 
+        oneodder.style.backgroundColor="green";
+        oneodder.style.color="white";
+       }
+     })
+
+     secodd.addEventListener("click",()=>{
+      if (oneodder.style.backgroundColor == green || thirdod.style.backgroundColor == green  || secodd.style.backgroundColor == green) { 
+        oneodder.style.backgroundColor=" rgb(207, 207, 207)"
+        oneodder.style.color=" black"
+        secodd.style.color=" white"
+        secodd.style.backgroundColor="green"; 
+        thirdod.style.backgroundColor="rgb(207, 207, 207)"
+        thirdod.style.color="black"
+      } 
+      
+      else{ 
+        secodd.style.backgroundColor="green";
+        secodd.style.color="white";
+       }
+     })
      lose.addEventListener('click',()=>{
        navigate1.style.width="0px"
      })
